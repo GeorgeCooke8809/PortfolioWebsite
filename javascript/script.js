84cbb71663dcf19bfc50007929f3dd8c1f5eb422
George Cooke <georgecooke8809@gmail.com>
2025-12-30 22:00:50 +0000

    Made it work when resizing the browser window

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,27 @@ function hideSidebar(){
     const hideButton = document.querySelector('.hide-side-bar-button')
     hideButton.style.display = 'none'
 }
+
+function checkSize(){
+    var width = window.innerWidth;
+    if (width > 1106){
+        expand()
+    }
+    else{
+        hideSidebar()
+    }
+}
+
+function expand(){
+    const sidebar = document.querySelector('.side-bar-navigation-list')
+    sidebar.style.display = 'none'
+    
+    const showButton = document.querySelector('.show-side-bar-button')
+    showButton.style.display = 'none'
+
+    const hideButton = document.querySelector('.hide-side-bar-button')
+    hideButton.style.display = 'none'
+
+    const menuItems = document.querySelector('.navigation-items')
+    menuItems.style.display = 'inline'
+}
